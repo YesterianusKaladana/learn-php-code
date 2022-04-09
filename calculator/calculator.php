6ee505c67fdfcb0a373c595d7e92d2fcdf6f628c
PAUL M YARANGGA <kyesterianus@gmail.com>
2022-04-09 11:49:25 +0900

    add file

---
+++ calculator/calculator.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+</head>
+<body>
+
+    <form>
+        <input type="text" name= "num1" placeholder= "Number 1">
+        <input type="text" name= "num2" placeholder= "Number 2">
+        <select name="operator">
+            <option >None </option>
+            <option >Add </option>
+            <option >Substract </option>
+            <option >Multiply </option>
+            <option >Divide </option>
+        </select>
+        <br>
+        <button type="submit" name="submit" value="submit">Calculate</button>
+    </form>
+
+    <p>The answer is:</p>
+
+    <?php
+    if () {
+        
+    }
+
+
+    ?>
+    
+</body>
+</html>
