6e09784b5a28c56274477b24a1357bb037093174
PAUL M YARANGGA <kyesterianus@gmail.com>
2022-04-09 11:14:20 +0900

    add file

--- switch-statement/switch.php
+++ switch-statement/switch.php
@@ -3,11 +3,22 @@
 
     <?php
 
-        $x = 1;
+        $x = 8;
         switch ($x) {
             case 1:
                 echo "The answer is 1";
         break;
+        case 2:
+            echo "The answer is 2";
+        break;
+        case 3:
+            echo "The answer is 3";
+         break;
+         case 4:
+        echo "The answer is 4";
+        break;
+        default:
+            echo "There is no answer";
         }
     ?>
     
