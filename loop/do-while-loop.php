9e5104533765cde8d4b956ea44c134851efc97a2
Yesterianus Kaladana <kyesterianus@gmail.com>
2022-04-12 23:30:49 +0900

    add file

---
+++ loop/do-while-loop.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+
+    <?php
+
+    //Loop
+
+    //While loop
+    //Do while loop
+    //For loop
+    //Foreach loop
+    $x = 10;
+    do {
+        echo "Hi there <br>";
+        $x++;
+    } while ($x <= 5);
+
+
+    ?>
+
+
+</body>
+
+</html>
