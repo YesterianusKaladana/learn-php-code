e8bdf8298534db66a39f90d97b14b4d790471c82
Yesterianus Kaladana <kyesterianus@gmail.com>
2022-04-13 00:01:32 +0900

    add file

---
+++ loop/foreach-loop.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+</head>
+
+<body>
+
+    <?php
+
+    //While loop
+    //Do while loop
+    //For loop
+    //Foreach loop
+
+    $array = array("Daniel", "Jane", "Jacob");
+
+    foreach ($array as $loopdata) {
+        echo "My name is " . $loopdata . "<br>";
+    }
+
+    ?>
+
+</body>
+
+</html>
