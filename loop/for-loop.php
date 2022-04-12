8d9f9fde4d6f7f74e498fad9a65b7fdf5e11268f
Yesterianus Kaladana <kyesterianus@gmail.com>
2022-04-12 23:48:55 +0900

    add file

---
+++ loop/for-loop.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+</head>
+
+<body>
+
+    <?php
+
+    //While loop
+    //Do while loop
+    //For loop
+    //Foreach loop
+
+    for ($x = 0; $x <= 10; $x++) {
+        echo "Hi <br>";
+    }
+
+    ?>
+
+</body>
+
+</html>
