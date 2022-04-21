bb7654bd62563c6206eb4c656614b65bb3c56ca4
Yesterianus Kaladana <kyesterianus@gmail.com>
2022-04-21 16:15:10 +0900

    add file

---
+++ arrays/arrays.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+</head>
+
+<body>
+
+    <?php
+
+    $arrays = array("Daniel", "Jane", "Jacob", "Jhon", "Mariane");
+
+    echo $array[2];
+
+    ?>
+
+</body>
+
+</html>
