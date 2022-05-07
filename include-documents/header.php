b62eff10cefda8dc4ad49f2b4eb3264c141b4217
PAUL M YARANGGA <kyesterianus@gmail.com>
2022-05-07 10:20:48 +0900

    add file

---
+++ include-documents/header.php
@@ -0,0 +1,24 @@
+<?php
+    include "user-functions.php";
+?>
+
+
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>How to Include Documents in PHP</title>
+    <link rel="stylesheet" type ="text/css" href="style.css">
+</head>
+<body>
+
+    <header>
+        <nav> 
+            <ul>
+                <li><a href="">nav1</a></li>
+                <li><a href="">nav2</a></li>
+                <li><a href="">nav3</a></li>
+                <li><a href="">nav4</a></li>
+            </ul>
+        </nav>
+    </header>
