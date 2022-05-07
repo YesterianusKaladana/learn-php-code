747a2444475c37e8b51460a30c7fa48773369387
PAUL M YARANGGA <kyesterianus@gmail.com>
2022-05-07 10:33:47 +0900

    add file

---
+++ local-and-global-scope/local-global.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Local and Global Scope in PHP</title>
+</head>
+<body>
+
+    <?php
+
+    $x = 5;
+
+    function something() {
+        $y = 10;
+
+    }
+
+    echo $x;
+    ?>
+    
+</body>
+</html>
