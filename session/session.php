0432d93070381f148d2e602d61549fa5871cbc65
PAUL M YARANGGA <kyesterianus@gmail.com>
2022-05-07 11:43:08 +0900

    add file

---
+++ session/session.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title> How to Start a Session in PHP</title>
+    <link rel="stylesheet" stye="text/css" href="style.css">
+</head>
+<body>
+
+    <ul>
+        <li><a href="index.php">HOME</a></li>
+        <li><a href="contact.php">CONTACT</a></li>
+    </ul>
+
+    <?php
+    
+    ?>
+    
+</body>
+</html>
