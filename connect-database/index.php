cd8a1ff18fd2977e42eee91b79d2241e26358692
PAUL M YARANGGA <kyesterianus@gmail.com>
2022-05-14 11:37:46 +0900

    add includes

---
+++ connect-database/index.php
@@ -0,0 +1,15 @@
+<?php
+    include_once 'includes/dbh.php';
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>How to connect to a database in PHP</title>
+</head>
+<body>
+
+    $conn;
+    
+</body>
+</html>
